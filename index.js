782260c308d56b8fa8f93859b8c10aac52bb38b5
DevRamona <r.ingabire@alustudent.com>
2024-06-25 17:55:00 +0200

    generate random jokes

---
+++ index.js
@@ -0,0 +1,15 @@
+const para = document.getElementById("text");
+const button = document.getElementById("btn");
+
+const getJoke = async() => {
+    const fetchedData = await fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit`);
+    const response = await fetchedData.json();
+    // console.log(response)
+    if(response.type === "single"){
+        para.textContent = response.joke
+    } else if(response.type === "twopart") {
+        para.textContent = `${response.setup} ${response.delivery}`
+    }
+    button.addEventListener("click", getJoke);
+}
+getJoke()
